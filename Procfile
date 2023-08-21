e5627bff993ab78990ebb7a5d291c1611c60bf0f
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-08-21 12:07:19 +0300

    ‘railway-config’

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'marvyn-kiplagat.wsgi'
--- config/settings.py
+++ config/settings.py
@@ -123,5 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 ALLOWED_HOSTS = ['*']
